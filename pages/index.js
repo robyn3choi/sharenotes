cfbd260a6d1324f2ba2406466f4f0521dbd3a4cc
Robyn Choi <robyn3choi@gmail.com>
2021-04-12 16:48:57 -0700

    testing if this fixes deploy

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,10 @@ import styles from '../styles/Home.module.css';
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch(`http://localhost:3001`);
-  const data = await res.json();
+  // const res = await fetch(`http://localhost:3001`);
+  // const data = await res.json();
   // Pass data to the page via props
-  return { props: { initialNoteValue: data.value } };
+  return { props: { initialNoteValue: 'blah' } };
 }
 
 export default function Home({ initialNoteValue }) {
