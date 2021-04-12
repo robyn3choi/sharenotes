5f8a1bb0888e8e6ceb61b54446825092f2a09b20
Robyn Choi <robyn3choi@gmail.com>
2021-04-12 16:39:00 -0700

    made into PWA

--- utils/WebSocketService.js
+++ utils/WebSocketService.js
@@ -10,6 +10,7 @@ export default class WebSocketService {
     this.ws = new WebSocket('ws://localhost:3001');
     this.ws.onopen = () => console.log('OPENED CONNECTION');
     this.ws.onmessage = (event) => this.handleReceiveMessage(event);
+    this.ws.onclose = (event) => console.log('CLOSED CONNECTION');
   }
 
   handleReceiveMessage(event) {
